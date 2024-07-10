34d1eebabaaf22ca622c112420c532b92ad60b10
MingiJ <jeffawesome.ja@gmail.com>
2024-07-10 11:54:57 +0300

    feat: incomplete sign up

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,3 +1,27 @@
+import { prisma } from '@/utils/db'
+import { auth, currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.clerkId,
+        email: user.email,
+        createdAt: user.createdAt,
+      },
+    })
+  }
+
+  redirect('/journal')
+}
 const NewUser = () => {
   return <div>hi</div>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
-
 export default async function Home() {
+  const { userId } = await auth()
+
+  let href = userId ? '/journal' : '/new-user'
   return (
     <div className="text-white w-screen h-screen bg-black flex justify-center items-center">
       <div className="max-w-[600px] mx-auto">
@@ -10,7 +13,7 @@ export default async function Home() {
           you have to do is be honest
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
